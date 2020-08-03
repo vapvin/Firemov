86a870c2f9532dab6cd27d65b5549d6eeb20ecdd
vapvin <vins@vins.dev>
2020-08-03 23:52:14 +0900

    Images Preload

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {AppLoading} from "expo";
+import { Image } from "react-native";
+import {Asset} from "expo-asset";
+
+const cacheImages = (images) => images.map(image => {
+  if(typeof image === "string"){
+    return Image.prefetch(image);
+  }else{
+    return Asset.fromModule(image).downloadAsync();
+  }
+});
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+  const loadAssets = async () => {
+    const images = cacheImages([
+        "https://images.unsplash.com/photo-1596431810482-2aa5de12ee5b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        require("./assets/splash.png")
+    ]);
+    console.log(images);
+  };
+  const onFinish = () => setReady(true);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    ready ? null : <AppLoading startAsync={loadAssets} onFinish={onFinish} onError={console.error} />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
