83efd3ca604e661628b208dd84343e8dc8252420
vapvin <tpehdj12@daum.net>
2020-08-04 16:15:00 +0900

    Change tab navigator header fuck are good this thing

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useLayoutEffect} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Movies from "../screens/Movies";
 import TV from "../screens/TV";
@@ -7,12 +7,19 @@ import Favs from "../screens/Favs";
 
 const Tabs = createBottomTabNavigator();
 
-export default () => (
-    <Tabs.Navigator>
-        <Tabs.Screen name="Movies" component={Movies} />
-        <Tabs.Screen name="TV" component={TV} />
-        <Tabs.Screen name="Search" component={Search} />
-        <Tabs.Screen name="Favs" component={Favs} />
-    </Tabs.Navigator>
-)
+const getNames = route => route?.state?.routeNames[route.state.index] || "Movies";
+
+export default ({navigation, route}) => {
+    useLayoutEffect(() => {
+       navigation.setOptions({title: getNames(route)});
+    }, [route])
+    return (
+        <Tabs.Navigator>
+            <Tabs.Screen name="Movies" component={Movies}/>
+            <Tabs.Screen name="TV" component={TV}/>
+            <Tabs.Screen name="Search" component={Search}/>
+            <Tabs.Screen name="Favourites" component={Favs}/>
+        </Tabs.Navigator>
+    )
+}
 
